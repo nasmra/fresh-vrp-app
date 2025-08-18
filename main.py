47d368ec800797f4b435bc5ac0189232a10dbdb1
nasmra <nassimstingo@gmail.com>
2025-08-18 19:49:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -451,7 +451,7 @@ def _generate_pdf_tours(assigned: list, df_geo: pd.DataFrame, orders_df: pd.Data
 #                    CONFIG / STYLES
 # =========================================================
 st.set_page_config(layout="wide", page_title="Gestion & optimisation des tournées")
-inject_brand_css()  # <= ICI : en dernier pour qu'il prenne le dessus
+#inject_brand_css()  # <= ICI : en dernier pour qu'il prenne le dessus
 
 st.markdown(
     """
@@ -1356,6 +1356,7 @@ with tab_add:
             except Exception as e:
                 with col_left:
                     st.error(f"❌ Échec d'écriture sur Drive : {e}")
+
 
 
 
