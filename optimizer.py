812123635c85060499b065eb93ec84827ba3901f
nasmra <nassimstingo@gmail.com>
2025-08-20 00:05:52 +0200

    Update optimizer.py

--- optimizer.py
+++ optimizer.py
@@ -262,7 +262,7 @@ def run_optimization(
 
     balance_span: bool = True,   # équilibrage des distances
     span_coeff: int = 50,        # 0..200 (baisser pour routes plus compactes)
-    time_limit_s: int = 300      # temps de recherche
+    time_limit_s: int = 60      # temps de recherche
 ):
     # ---------- Lecture sources ----------
     dist_mat_raw = pd.read_excel(distance_file, index_col=0)
@@ -536,3 +536,4 @@ def run_optimization(
 
     result_str += f"\nTotal : {int(round(total_d))} km | {total_w:.1f} kg | {total_c:.1f} cartons"
     return result_str, out
+
